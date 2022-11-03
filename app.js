690669e6d859108ee44d0973c99630ad870d9351
Gabriell Gardner <gabriell.github@gmail.com>
2022-11-03 16:48:15 -0500

    Added initial starter files

---
+++ app.js
@@ -0,0 +1,37 @@
+window.onload = function() 
+{
+
+    var searchBtn = document.querySelector('#searchBtn');
+    var httpRequest;
+  
+    searchBtn.addEventListener('click', function(element) 
+    {
+      element.preventDefault();
+  
+      httpRequest = new XMLHttpRequest();
+  
+      // GET Request
+      var url = "superheroes.php";
+      httpRequest.onreadystatechange = heroList;
+      httpRequest.open('GET', url);
+      httpRequest.send();
+    });
+  
+    function heroList() 
+    {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) 
+      {
+        if (httpRequest.status === 200) 
+        {
+            let response = httpRequest.responseText;
+            alert(response);
+
+        } 
+        else 
+        {
+          alert('There was a problem with the request.');
+        }
+      }
+    }
+  
+};
